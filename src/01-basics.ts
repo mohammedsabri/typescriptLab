199e3f319550b42604388d386ea97d7f4801ddeb
mohammed sabri <mohammedsabri228@gmail.com>
2025-01-30 12:56:09 +0000

    Complete type-safety to colleagues data

--- src/01-basics.ts
+++ src/01-basics.ts
@@ -1,58 +1,68 @@
-// (Optional) Change the names below to your friends.
-
 interface Friend {
     name: string;
     phone: string;
-    age: number
+    age: number;
 }
 
+interface Colleague {
+    name: string;
+    department: string;
+    contact: {
+        email: string;
+        extension: number;
+    };
+}
 
-const friend1 = {
+interface ColleagueHistory {
+    current: Colleague[];
+    former: Colleague[];
+}
+
+const friend1: Friend = {
     name: "Paul Fleming",
     phone: "087-12345",
     age: 25,
-  };
-  
-  const friend2 = {
+};
+
+const friend2: Friend = {
     name: "Jane Costello",
-    phone: "086--12345",
+    phone: "086-12345",
     age: 31,
-  };
-  
-  const friends = [friend1, friend2];
-  console.log(friends[1]);
-  
-  //   -------------------
-  const colleague1 = {
+};
+
+const friends: Friend[] = [friend1, friend2];
+console.log(friends[1]);
+
+const colleague1: Colleague = {
     name: "Ralph Graham",
     department: "Engineering",
     contact: {
-      email: "rgraham@company.com",
-      extension: 121,
+        email: "rgraham@company.com",
+        extension: 121,
     },
-  };
-  
-  const colleague2 = {
+};
+
+const colleague2: Colleague = {
     name: "Patti Burke",
     department: "Finance",
     contact: {
-      email: "pburke@company.com",
-      extension: 132,
+        email: "pburke@company.com",
+        extension: 132,
     },
-  };
-  
-  const colleague3 = {
+};
+
+const colleague3: Colleague = {
     name: "Dean Sullivan",
     department: "HR",
     contact: {
-      email: "dos@company.com",
-      extension: 125,
+        email: "dos@company.com",
+        extension: 125,
     },
-  };
-  const colleagues = {
+};
+
+export const colleagues: ColleagueHistory = {
     current: [colleague1, colleague2, colleague3],
     former: [],
-  };
-  
-  console.log(colleagues.current[0]);
-  
+};
+
+console.log(colleagues.current[0]);
